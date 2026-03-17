a4e7bfb1723f6ed469d916ee6b2f2b134a408bec
progamercoder2 <stroganovaniton@gmail.com>
2026-03-17 19:05:53 +0300

    Обработка главного меню

--- handlers/h03_order.py
+++ handlers/h03_order.py
@@ -1,4 +1,5 @@
 from aiogram import Router, F, Bot
+from aiogram.exceptions import TelegramBadRequest
 from aiogram.types import Message
 from handlers.h02_getcontact import show_main_menu
 from keyboards.inline import generate_category_menu
@@ -13,3 +14,12 @@ async def make_order(message: Message, bot):
     chat_id = message.chat.id
     await bot.send_message(chat_id=chat_id, text="Формирование заказа", reply_markup=back_to_main_menu())
     await message.answer(text="Выберите категорию", reply_markup=generate_category_menu(chat_id))
+
+@router.message(F.text == "Главное меню")
+async def handle_main_menu(message: Message, bot:Bot):
+    """переход в главное меню с удалением прошлых действий"""
+    try:
+        await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id - 1)
+    except TelegramBadRequest:
+        pass
+    await show_main_menu(message)
