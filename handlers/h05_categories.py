7fcbb22bac146a46023f35f8ed5ad7bef1cb13ef
progamercoder2 <stroganovaniton@gmail.com>
2026-04-14 20:12:57 +0300

    Возрат к списку категорий

--- handlers/h05_categories.py
+++ handlers/h05_categories.py
@@ -2,9 +2,10 @@ from aiogram import Router, F
 from aiogram.exceptions import TelegramBadRequest
 from aiogram.types import CallbackQuery
 
-from keyboards.inline import show_product_by_category
+from keyboards.inline import show_product_by_category, generate_category_menu
 
 router = Router()
+
 
 @router.callback_query(F.data.regexp(r'^category_(\d+)$'))
 async def show_product(callback: CallbackQuery):
@@ -21,3 +22,16 @@ async def show_product(callback: CallbackQuery):
         )
     except TelegramBadRequest:
         await callback.answer("Не удалось открыть выбранную категорию")
+
+@router.callback_query(F.data=="return_to_category")
+async def return_to_category(callback:CallbackQuery):
+    """Возрат к списку категорий"""
+    chat_id=callback.message.chat.id
+    message_id=callback.message.message_id
+
+    await callback.bot.edit_message_text(
+        text = 'Выберите категорию',
+        message_id=message_id,
+        chat_id=chat_id,
+        reply_markup=generate_category_menu(chat_id)
+    )
