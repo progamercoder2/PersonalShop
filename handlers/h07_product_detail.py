606cdb7e4f4bde952a7e33253f91cb4b77fb7d03
progamercoder2 <stroganovaniton@gmail.com>
2026-06-16 20:05:56 +0300

    написали функцию для возврата из просмотра информации к категориям в хендлере

--- handlers/h07_product_detail.py
+++ handlers/h07_product_detail.py
@@ -2,14 +2,15 @@ from aiogram import Router, F, Bot
 from aiogram.types import CallbackQuery, FSInputFile
 
 from bot_utils.message_caption import text_for_caption
-from database.utils import db_get_product_by_id, db_get_user_cart, db_add_or_update_item
-from keyboards.inline import quantity_cart_controls
+from database.utils import db_get_product_by_id, db_get_user_cart, db_add_or_update_item, db_get_all_category
+from keyboards.inline import quantity_cart_controls, generate_category_menu
+from keyboards.reply import phone_button
 
 router = Router()
 
 
 @router.callback_query(F.data.startswith('product_view_'))
-async def show_product_view(callback: CallbackQuery, bot: Bot):
+async def show_product_info(callback: CallbackQuery, bot: Bot):
     """показ детальной информации о продукте"""
     chat_id = callback.message.chat.id
     message_id = callback.message.message_id
@@ -50,4 +51,24 @@ async def show_product_view(callback: CallbackQuery, bot: Bot):
 
 async def ask_for_phone(chat_id: int, bot: Bot):
     """запрос номера телефона для неавторизованного пользователя"""
-        await bot.send_message(chat_id=chat_id, text='оставьте номер телефона', reply_markup=phone_button())
+    await bot.send_message(chat_id=chat_id, text='оставьте номер телефона', reply_markup=phone_button())
+
+
+@router.callback_query(F.data == "from_detail_to_category")
+async def handle_back_to_category(callback: CallbackQuery, bot: Bot):
+    """возврат из просмотра информации к категории"""
+    chat_id = callback.message.chat.id
+    message_id = callback.message.message_id
+    try:
+        await bot.delete_message(chat_id=chat_id, message_id=message_id)
+    except Exception as e:
+        print(e)
+
+    categories = db_get_all_category()
+    if not categories:
+        await bot.send_message(chat_id=chat_id, text='нет категорий')
+        return
+
+    keyboard = generate_category_menu(chat_id)
+    await bot.send_message(chat_id=chat_id,text="Выберите категорию", reply_markup=keyboard)
+    await callback.answer()
--- keyboards/inline.py
+++ keyboards/inline.py
@@ -40,6 +40,6 @@ def quantity_cart_controls(quantity=1):
     builder.button(text=str(quantity), callback_data='quantity')
     builder.button(text='➕', callback_data='action +')
     builder.button(text='положить в корзину', callback_data='put_in_cart')
-    builder.button(text='◀️ Назад', callback_data='from_detail_to')
+    builder.button(text='◀️ Назад', callback_data='from_detail_to_category')
     builder.adjust(3, 1, 1)
     return builder.as_markup(resize_keyboard=True)
